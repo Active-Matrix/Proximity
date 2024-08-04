31d9c4c3f3f023bfa1ea918899e23558c6a8d014
Akshay Vs <me.akshay.vs@gmail.com>
2024-08-04 15:02:26 +0530

    feat: -r web/hooks updated useWindowSize to decide weather the devide is mobile or not

--- src/web/hooks/useWindowSize.ts
+++ src/web/hooks/useWindowSize.ts
@@ -1,25 +1,18 @@
-//? This hook provides the current width and height of the browser window.
-
 import { useState, useLayoutEffect } from "react";
 
-export const useWindowSize = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
+// This hook determine if the current view is mobile/tablet
+export const useIsMobile = (breakpoint = 1024) => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= breakpoint);
 
   useLayoutEffect(() => {
-    function handleResize() {
-      setWindowSize({
-        width: window.innerWidth,
-        height: window.innerHeight
-      });
-    }
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= breakpoint);
+    };
 
     window.addEventListener("resize", handleResize);
 
     return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  }, [breakpoint]);
 
-  return windowSize;
+  return isMobile;
 };
