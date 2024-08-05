eca883c7ba5c41ce2a14acc8f052eef982f8dcb3
Akshay Vs <me.akshay.vs@gmail.com>
2024-08-05 17:38:22 +0530

    fix: -r web/hooks fixed window not found prerender error

--- src/web/components/layout/Main.tsx
+++ src/web/components/layout/Main.tsx
@@ -1,7 +1,7 @@
 import React, { PropsWithChildren } from 'react';
 
 const Main = ({ children }: PropsWithChildren) => {
-  return <main className="flex flex-col gap-5 pb-32">{children}</main>;
+  return <main className="flex flex-col gap-5 lg:gap-8 pb-32">{children}</main>;
 };
 
 export default Main;
