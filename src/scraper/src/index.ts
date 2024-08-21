6cd2cbe4abe13a0b9fc696eedb6e33f9d9931aa1
Akshay Vs <me.akshay.vs@gmail.com>
2024-08-21 20:28:09 +0530

    feat(scraper): exposed scraper service to port 8181

--- src/scraper/src/index.ts
+++ src/scraper/src/index.ts
@@ -1,64 +1,42 @@
 import PuppeteerScraper, { ScraperConfig } from "./Scraper";
-import fs from "node:fs"
-import { OmitExcept } from "../utils/OmitExcept";
-import { Config } from "@jest/types";
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const config: ScraperConfig = {
-  provider: "BBC",
-  container: [{
-    element: 'div',
-    attribute: 'data-testid',
-    value: 'edinburgh-card',
-  }],
-  title: [
-    {
-      element: 'h2',
-      attribute: 'data-testid',
-      value: 'card-headline'
+dotenv.config();
+
+const app = express();
+const PORT = 8181;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.EXPRESS_CLIENT_ORIGIN,
+  })
+);
+
+app.get("/api/v1/scrape", async (req: Request, res: Response) => {
+  try {
+    if (!req.body || !req.body.url || !req.body.config) {
+      return res.status(400).json({ message: "Missing required parameters: url and config" });
     }
-  ],
-  image: [
-    {
-      element: 'img',
-      attribute: 'data-testid',
-      value: 'card-media',
-      resultAttribute: 'src'
+
+    const { url, config }: { url: string; config: ScraperConfig } = req.body;
+
+    console.log("Starting scraper", config.provider);
+
+    const scraper = new PuppeteerScraper(url, config);
+    const scraperData = await scraper.scrape();
+
+    if (!scraperData) {
+      return res.status(500).json({ message: "Failed to scrape data" });
     }
-  ],
-  href: [
-    {
-      element: 'a',
-      attribute: 'data-testid',
-      value: 'internal-link',
-      resultAttribute: 'href'
-    }
-  ],
-  lastUpdated: [
-    {
-      element: 'span',
-      attribute: 'data-testid',
-      value: 'card-metadata-lastupdated',
-    }],
-  description: [
-    {
-      element: 'p',
-      attribute: 'data-testid',
-      value: 'card-description',
-    }
-  ]
-}
 
-const url = "https://www.bbc.com/news"
+    res.json({ data: scraperData, url, config });
+  } catch (error: any) {
+    console.error("Error while scraping", error.message, error.stack);
+    res.status(500).json({ message: "Failed to scrape data" });
+  }
+});
 
-const main = async () => {
-
-  const scraper = new PuppeteerScraper(url, config);
-  const scrapedData = await scraper.scrape();
-
-  fs.writeFile('./.dist/artifacts.json', JSON.stringify(scrapedData), err => {
-    console.log(err);
-  });
-}
-
-
-main() 
+app.listen(PORT, () => console.log("Scraper Service started listening on port:", PORT))
